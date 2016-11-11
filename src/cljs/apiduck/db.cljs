a74abe0e3e18760f5fc2b313c5a1683acc8eddc8
Mingqian Ye <mingqian.ye@33across.com>
2016-11-11 14:06:27 -0800

    work

--- src/cljs/apiduck/db.cljs
+++ src/cljs/apiduck/db.cljs
@@ -6,9 +6,33 @@
   "description" "A product from Acme's catalog"
   "type" "object"
   "properties" {
-      "id"    { "title" "Product id" "description" "The identifier for a product" "type" "integer" }
-      "name"  { "title" "Product name" "description" "Name of the product" "type" "string" }
-      "price" { "title" "Product price" "description" "The price" "type" "number" }
+      "id"    { "title" "Product id" 
+                "description" "The identifier for a product" 
+                "type" "integer" }
+      "name"  { "title" "Product name" 
+                "description" "Name of the product" 
+                "type" "string" }
+      "price" { "title" "Product price" 
+                "description" "The price" 
+                "type" "number" }
+
+      "address" { "title" "Product address" 
+                  "description" "The address of the product shipping address"
+                  "type" "object"
+                  "properties" {
+                     "city" {
+                        "title" "City name"
+                        "description" "The name of the address city"
+                        "type" "string"
+                            }
+                     "country"  {
+                        "title" "Country name"
+                        "description" "The name of address country"
+                        "type" "string"
+                                }
+                               }
+                }
+
   }
   })
 
