d19aa78684cc84cbe906ac2f5b84b3ceb0a769e1
Mingqian Ye <mingqian.ye@33across.com>
2016-11-10 18:38:14 -0800

    work

---
+++ src/cljs/apiduck/components/duck_row.cljs
@@ -0,0 +1,45 @@
+(ns apiduck.duck-row
+  (:require [re-com.core                   :refer [row-button checkbox]
+                                           :refer-macros [handler-fn]]))
+
+(defn data-row
+  [row first? last? mouse-over click-msg]
+  (let [mouse-over-row? (identical? @mouse-over row)]
+  [:tr {:on-mouse-over (handler-fn (reset! mouse-over row))
+        :on-mouse-out  (handler-fn (reset! mouse-over nil))}
+        [:td 
+          [row-button
+                          :md-icon-name    "zmdi zmdi-arrow-back zmdi-hc-rotate-90"
+                          :mouse-over-row? mouse-over-row?
+                          :tooltip         "Move this line up"
+                          :disabled?       (and first? mouse-over-row?)
+                          :on-click        #(reset! click-msg (str "move row " (:id row) " up"))
+          ]
+          [row-button
+                           :md-icon-name    "zmdi zmdi-arrow-forward zmdi-hc-rotate-90"
+                           :mouse-over-row? mouse-over-row?
+                           :tooltip         "Move this line down"
+                           :disabled?       (and last? mouse-over-row?)
+                           :on-click        #(reset! click-msg (str "move row " (:id row) " down"))
+          ]] 
+       [:td (:name row)] 
+       [:td (:from row)] 
+       [:td (:to row)] 
+       [:td 
+         [row-button
+          :md-icon-name    "zmdi zmdi-copy"
+          :mouse-over-row? mouse-over-row?
+          :tooltip         "Copy this line"
+          :on-click        #(reset! click-msg (str "copy row " (:id row)))]
+         [row-button
+          :md-icon-name    "zmdi zmdi-edit"
+          :mouse-over-row? mouse-over-row?
+          :tooltip         "Edit this line"
+          :on-click        #(reset! click-msg (str "edit row " (:id row)))]
+         [row-button
+          :md-icon-name    "zmdi zmdi-delete"
+          :mouse-over-row? mouse-over-row?
+          :tooltip         "Delete this line"
+          :on-click        #(reset! click-msg (str "delete row " (:id row)))]
+        ]
+       ]))
--- src/cljs/apiduck/components/duck_table.cljs
+++ src/cljs/apiduck/components/duck_table.cljs
@@ -1,64 +1,40 @@
 (ns apiduck.duck-table
-  (:require [re-com.core                   :refer [h-box v-box row-button label checkbox p]
-                                           :refer-macros [handler-fn]]
-            [re-com.util                   :refer [enumerate]]
-            [reagent.core                  :as    reagent]))
+  (:require [reagent.core                  :as    reagent]
+            [apiduck.duck-row              :refer [data-row]]))
 
-(defn myrow
-  [row first? last? mouse-over click-msg]
-  (let [mouse-over-row? (identical? @mouse-over row)]
-  [:tr {:on-mouse-over (handler-fn (reset! mouse-over row))
-        :on-mouse-out  (handler-fn (reset! mouse-over nil))}
-        [:td 
-          [row-button
-                          :md-icon-name    "zmdi zmdi-arrow-back zmdi-hc-rotate-90"
-                          :mouse-over-row? mouse-over-row?
-                          :tooltip         "Move this line up"
-                          :disabled?       (and first? mouse-over-row?)
-                          :on-click        #(reset! click-msg (str "move row " (:id row) " up"))
-          ]
-          [row-button
-                           :md-icon-name    "zmdi zmdi-arrow-forward zmdi-hc-rotate-90"
-                           :mouse-over-row? mouse-over-row?
-                           :tooltip         "Move this line down"
-                           :disabled?       (and last? mouse-over-row?)
-                           :on-click        #(reset! click-msg (str "move row " (:id row) " down"))
-          ]] 
-       [:td (:name row)] 
-       [:td (:from row)] 
-       [:td (:to row)] 
-       [:td 
-         [row-button
-          :md-icon-name    "zmdi zmdi-copy"
-          :mouse-over-row? mouse-over-row?
-          :tooltip         "Copy this line"
-          :on-click        #(reset! click-msg (str "copy row " (:id row)))]
-         [row-button
-          :md-icon-name    "zmdi zmdi-edit"
-          :mouse-over-row? mouse-over-row?
-          :tooltip         "Edit this line"
-          :on-click        #(reset! click-msg (str "edit row " (:id row)))]
-         [row-button
-          :md-icon-name    "zmdi zmdi-delete"
-          :mouse-over-row? mouse-over-row?
-          :tooltip         "Delete this line"
-          :on-click        #(reset! click-msg (str "delete row " (:id row)))]
+(defn decorated-rows
+  [coll mouse-over click-msg]
+  (let [c (dec (count coll))]
+  (->> coll
+      (sort-by :sort)
+      (map-indexed (fn [index item] [item (= 0 index) (= c index) mouse-over click-msg]))
+      println
+  )))
+
+(defn enumerate
+  "(for [[item first? last?] (enumerate coll)] ...)  "
+  [coll]
+  (let [c (dec (count coll))
+        f (fn [index item] [item (= 0 index) (= c index)])
         ]
-       ]))
+    (map-indexed f (sort-by :sort coll))))
 
-(defn mytable
+
+(defn data-table
   [rows]
   (let [mouse-over (reagent/atom nil)
         click-msg  (reagent/atom "")]
   (fn []
-    [:div {:class "table-responsive"}
+    [:div
       [:table {:class "table table-hover table-inverse"}
         [:thead          
           [:tr [:th "Sort"] [:th "Name"] [:th "From"] [:th "To"] [:th "Actions"]]
         ]
         [:tbody
-          (for [[_ row first? last?] (enumerate (sort-by :sort rows))]
-            ^{:key (:id row)} [myrow row first? last? mouse-over click-msg])]
+          
+          (for [[row first? last?] (enumerate rows)]
+            ^{:key (:id row)} [data-row row first? last? mouse-over click-msg])
+        ]
       ]
       [:div (str "clicked: " @click-msg)]
     ])))
@@ -71,7 +47,7 @@
                     {:id "3" :sort 2 :name "Time range 3" :from "06:00" :to "18:00"}]]
     (fn []
       [:div
-        [mytable rows]])))
+        [data-table rows]])))
 
 
 ;; core holds a reference to panel, so need one level of indirection to get figwheel updates
