5fad81e167d4067b920ea133a830a56500a0029a
Mingqian Ye <mingqian.ye@33across.com>
2016-11-15 18:08:09 -0800

    work

--- src/cljs/apiduck/components/table_pane.cljs
+++ src/cljs/apiduck/components/table_pane.cljs
@@ -1,24 +1,24 @@
 (ns apiduck.components.table-pane
   (:require [re-frame.core    :as re-frame]
             [reagent.core     :as    reagent]
+            [re-frame.core :refer [dispatch]]
             [re-com.core :refer [md-icon-button]
                          :refer-macros [handler-fn]]
             [apiduck.components.duck-table :refer [table]]))
 
 (defn add-root-element-button
   []
-  (let [hover? (reagent/atom false)]
+  (let [schema (re-frame/subscribe [:current-schema])]
   (fn []
     [md-icon-button
       :md-icon-name    "zmdi zmdi-plus-square"
       :class           "mdc-text-green"
       :tooltip         "Add Property"
-      :on-click        #()] )))
+      :on-click        #(dispatch [:add-row (:block-id @schema)])])))
 
 (defn table-pane
   []
   [:div
     [table]
-    [add-root-element-button]
-  ]
+    [add-root-element-button]]
   )
