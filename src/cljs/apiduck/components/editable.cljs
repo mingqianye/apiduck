575b85ca6e74314c8bab3c2f9eaec8deb2a9355e
Mingqian Ye <mingqian.ye@33across.com>
2016-11-17 18:26:57 -0800

    work

--- src/cljs/apiduck/components/editable.cljs
+++ src/cljs/apiduck/components/editable.cljs
@@ -6,14 +6,16 @@
   [& {:keys [value on-save]}]
   (let [text          (reagent/atom value)
         edit-mode     (reagent/atom false)
-        save-and-exit #(do (on-save @text) (reset! edit-mode false))]
+        save-and-exit #(do (reset! text (clojure.string/trim @text))
+                           (on-save @text) 
+                           (reset! edit-mode false))]
         
     (fn []
       (if @edit-mode
         [:input {:value      @text
                  :type       "text"
                  :auto-focus true
-                 :on-change  #(reset! text (-> % .-target .-value clojure.string/trim))
+                 :on-change  #(reset! text (-> % .-target .-value))
                  :on-blur    save-and-exit
                  :on-key-down #(case (.-which %)
                                  13 (save-and-exit)
